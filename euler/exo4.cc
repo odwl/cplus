58122330876d4581cd94ba52e731154d01555397
odwl <odwl@google.com>
2009-08-21 14:57:24 +0200

    exo4

---
+++ euler/exo4.cc
@@ -0,0 +1,36 @@
+
+#include <iostream>
+
+using namespace std;
+
+bool IsPalindrome(int num) {
+  char numstr[10];
+  sprintf(numstr, "%d", num);
+  int len = string(numstr).size();
+  bool isPalindrom = true;
+  for (int i = 0; i < len; ++i) {
+    if (numstr[i] != numstr[len - i -1]) {
+     isPalindrom = false;
+     break;
+    }
+  }
+  return isPalindrom;
+}
+
+int Scan() {
+  int max = 0;
+  for (int i = 100; i < 1000; ++i) {
+    for (int j = i; j < 1000; ++j) {
+      int num = i * j;
+      if (max < num && IsPalindrome(num)) max = num;
+    }
+  }
+  return max;
+}
+
+int main() {
+  cout << "EXO 4" << endl;
+  cout << Scan();
+  cout << endl;
+  return 0;
+}
